4b9e51755860c468b211e4d379de47a90b35fc36
Rizky Agung Martadiraja <43920238+zexalryz@users.noreply.github.com>
2022-04-03 01:39:10 +0700

    fix

--- components/about.tsx
+++ components/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
             <div className="sm:flex bg-[#b13bbe] container items-center max-w-6xl px-8 mx-auto xl:px-5">
                 <div className="w-full pt-6 md:px-10 pt-6 w-full sm:w-full px-5 pt-6">
                     
-                    <img className="w-full h-auto overflow-hidden shadow-xl rounded-xl" src={gif.src} alt="Alpaca's" />
+                    <img className="w-full h-auto overflow-hidden shadow-xl rounded-xl" src={gif.src} alt="Alpacas" />
                 
                     
                 </div>
@@ -24,7 +24,7 @@ export default function About() {
                         <h1 className="text-[#fff] font-bold text-2xl  my-2 text-center">What is Alpaca Project</h1>
                         
                     <div className="container pt-4">
-                    <p className="text-[#fff] text-xl mb-2 md:mb-6">The Alpaca Project is made up of 999 Alpaca's. The vision of the project is to create a tight knit community where you can safely discuss all things Crypto/NFT with the goal of providing ongoing value to every holder and the community in general.</p>
+                    <p className="text-[#fff] text-xl mb-2 md:mb-6">The Alpaca Project is made up of 999 Alpacas. The vision of the project is to create a tight knit community where you can safely discuss all things Crypto/NFT with the goal of providing ongoing value to every holder and the community in general.</p>
                     <p className="text-[#fff] text-base mb-2 md:mb-6 pb-10">The way we aim to do this is through creation and curation of tools and resources that help people navigate this space.</p>
             
                     </div>
